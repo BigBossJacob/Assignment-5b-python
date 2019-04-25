4dc8507ce081a2e6a61f62b468b5ea3edd745475
BigBossJacob <47572661+BigBossJacob@users.noreply.github.com>
2019-04-25 18:20:33 -0400

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,93 @@
+madLib = 1
+
+print("Book MadLib")
+
+while madLib <= 22:
+
+	word1 = (input ("Choose an adjective: "))
+	madLib = madLib + 1
+
+	word2 = (input ("Choose a noun: "))
+	madLib = madLib + 1
+
+	word3 = (input ("Choose a plural noun: "))
+	madLib = madLib + 1
+
+	word4 = (input ("Choose a female name: "))
+	madLib = madLib + 1
+
+	word5 = (input ("Choose an adjective: "))
+	madLib = madLib + 1
+
+	word6 = (input ("Choose an article of clothing: "))
+	madLib = madLib + 1
+
+	word7 = (input ("Choose a noun: "))
+	madLib = madLib + 1
+
+	word8 = (input ("Choose a city: "))
+	madLib = madLib + 1
+
+	word9 = (input ("Choose a plural noun: "))
+	madLib = madLib + 1
+
+	word10 = (input ("Choose an adjective: "))
+	madLib = madLib + 1
+
+	word11 = (input ("Choose a part of the body: "))
+	madLib = madLib + 1
+
+	word12 = (input ("Choose a letter of the alphabet: "))
+	madLib = madLib + 1
+
+	word13 = (input ("Choose a celebrity: "))
+	madLib = madLib + 1
+
+	word14 = (input ("Choose a plural noun: "))
+	madLib = madLib + 1
+
+	word15 = (input ("Choose an adjective: "))
+	madLib = madLib + 1
+
+	word16 = (input ("Choose a place: "))
+	madLib = madLib + 1
+
+	word17 = (input ("Choose a part of the body: "))
+	madLib = madLib + 1
+
+	word18 = (input ("Choose an adjective: "))
+	madLib = madLib + 1
+
+	word19 = (input ("Choose an adjective: "))
+	madLib = madLib + 1
+
+	word20 = (input ("Choose a verb: "))
+	madLib = madLib + 1
+
+	word21 = (input ("Choose a plural noun: "))
+	madLib = madLib + 1
+
+	word22 = (input ("Choose a number: "))
+	madLib = madLib + 1
+
+	if madLib >= 23:
+		break
+if madLib >= 23:
+	print("")
+	print("Here is the madlib featuring the words you chose!")
+	print("")
+	print ("There are many", word1, "ways to choose a/an", word2, "to")
+	print("read. First, you ask for recommendations from your friends and")
+	print(word3, ". Just don't ask Aunt", word4, "-she only")
+	print("reads", word5, "books with", word6, "ripping goddesses")
+	print("on the cover. If your friends and family are no help, try checking out the")
+	print(word7, "Review in 'The", word8, "Times.' If the", word9)
+	print("featured there are too", word10, "for your taste, try something a little")
+	print("more low", word11, ", like '", word12, ": The", word13) 
+	print("Magazine', or '", word14, "Magazine'. You could also choose a book the")
+	print(word15,"-fashioned way. Head to your local library or", word16)
+	print("and browse the shelves until something catches your", word17,".")
+	print("Or, you could save yourself a whole lot of", word18, "trouble and log on")
+	print("to www.bookwish.com, the", word19, "new website to", word20, "for")
+	print("books! With all the time you'll save not having to search for", word21, ",")
+	print("you can read", word22, "more books!")
